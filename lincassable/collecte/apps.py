d668f32efe4df848e8140adae98a4c6ac777af5f
Benoit Guigal <benoit.guigal@protonmail.com>
2023-03-27 21:36:18 +0200

    bootstrap django app

---
+++ lincassable/collecte/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CollecteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'collecte'
---
+++ lincassable/collecte/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class PointDeCollecte(models.Model):
+    nom = models.CharField(max_length=200)
+    adresse = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nom
