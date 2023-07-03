c510229b4233ae82ea450aa832573fce8849225c
Benoit Guigal <benoit.guigal@protonmail.com>
2023-07-03 17:19:56 +0200

    create, update and delete views are working

--- lincassable/collecte/urls.py
+++ lincassable/collecte/urls.py
@@ -1,7 +1,32 @@
 from django.urls import path
+from collecte.views import (
+    PointDeCollecteListView,
+    PointDeCollecteCreateView,
+    PointDeCollecteUpdateView,
+    PointDeCollecteDeleteView,
+)
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(
+        "pointdecollecte/list/",
+        PointDeCollecteListView.as_view(),
+        name="pointdecollecte_list",
+    ),
+    path(
+        "pointdecollecte/add/",
+        PointDeCollecteCreateView.as_view(),
+        name="pointdecollecte_add",
+    ),
+    path(
+        "pointdecollecte/<int:pk>/",
+        PointDeCollecteUpdateView.as_view(),
+        name="pointdecollecte_update",
+    ),
+    path(
+        "pointdecollecte/<int:pk>/delete/",
+        PointDeCollecteDeleteView.as_view(),
+        name="pointdecollecte_delete",
+    ),
 ]
--- lincassable/collecte/views.py
+++ lincassable/collecte/views.py
@@ -1,8 +1,56 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import PointDeCollecte
+from django.urls import reverse_lazy
 
 
-def index(request):
-    point_de_collectes = PointDeCollecte.objects.all()
-    context = {"point_de_collectes": point_de_collectes}
-    return render(request, "collecte/index.html", context)
+class PointDeCollecteListView(ListView):
+    model = PointDeCollecte
+    context_object_name = "points_de_collecte"
+
+
+class PointDeCollecteCreateView(CreateView):
+    model = PointDeCollecte
+    fields = [
+        "nom",
+        "adresse",
+        "latitude",
+        "longitude",
+        "nom_contact_1",
+        "nom_contact_2",
+        "email_contact_1",
+        "email_contact_2",
+        "telephone_1",
+        "telephone_2",
+        "stock_casier",
+    ]
+    success_url = reverse_lazy("pointdecollecte_list")
+
+
+class PointDeCollecteUpdateView(UpdateView):
+    model = PointDeCollecte
+    fields = [
+        "nom",
+        "adresse",
+        "latitude",
+        "longitude",
+        "nom_contact_1",
+        "nom_contact_2",
+        "email_contact_1",
+        "email_contact_2",
+        "telephone_1",
+        "telephone_2",
+        "stock_casier",
+    ]
+    success_url = reverse_lazy("pointdecollecte_list")
+
+
+class PointDeCollecteDeleteView(DeleteView):
+    model = PointDeCollecte
+    success_url = reverse_lazy("pointdecollecte_list")
+
+
+def add_point_de_collecte(request):
+    return render(request, "collecte/add-point-de-collecte.html")
